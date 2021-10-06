dddfde4f2c99a6ce7c817a9e553c69a32bddbeac
Jonathan Rodrigues <jonrodrigues24@gmail.com>
2021-10-06 15:00:46 -0600

    code along CSS tips

---
+++ data-types/data-types.js
@@ -0,0 +1,3 @@
+const age = 18
+
+const zipcode = '87109'
---
+++ functions-1/functions-1.js
@@ -0,0 +1,42 @@
+// Function #1
+// Take two numbers as arguments and return a new value after a mathematical operation (addition, subtraction, multiplication, etc.) Which operation is your choice.
+
+//     Function #2
+// Take two strings as arguments and return a value that has them concatenated.
+
+
+
+
+function addition(first, second){
+    return(first * second);
+}
+
+console.log(addition(2,2));
+console.log(addition('car','dog'));
+
+
+function square(root){
+    return root * root;
+}
+
+console.log(square(5));
+
+
+
+function subtract(numberOne, numberTwo){
+    const difference = numberOne - numberTwo;
+    return difference;
+}
+
+console.log('margin', subtract(2000, 1000));
+console.log('profit', subtract(1000, 500));
+
+
+
+
+
+
+
+
+
+
